985b170631f7c5c98db6311f5ee8f5905d5274ce
kennyg37 <k.ganza@alustudent.com>
2023-07-19 12:01:24 +0200

    initial commit

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 
 file {'/home/kennyg37/.ssh/config':
   ensure  => file,
-  content => "Host your_server_hostname_or_ip\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
+  content => "ubuntu\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
   owner   => 'kennyg37',
   group   => 'kennyg37',
   mode    => '0600',
